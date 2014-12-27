275685d216f6ec89194706795b3c274b29b995ea
unknown <armaorob@yahoo.fr>
2014-12-28 00:25:49 +0100

    Add alias for items and blocks, change the work tree

--- bijoux/init.lua
+++ bijoux/init.lua
@@ -1,6 +1,7 @@
 -- Mod_Bijoux
 -- Mod créé par Ataron 
 
+dofile(minetest.get_modpath("bijoux").."/alia.lua")
 dofile(minetest.get_modpath("bijoux").."/atelier.lua")
 dofile(minetest.get_modpath("bijoux").."/function_register_items.lua")
 dofile(minetest.get_modpath("bijoux").."/function_register_nodes.lua")
