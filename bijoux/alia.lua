3741a4c231bd8929ac4beedcbfd681737f520956
unknown <armaorob@yahoo.fr>
2014-12-28 00:38:33 +0100

    Change an error in alia.lua, delete olds register_recipe in craft.lua, delete items.lua

--- bijoux/alia.lua
+++ bijoux/alia.lua
@@ -6,23 +6,23 @@ minetest.register_alias("bijoux:emeraude_bloc", "bijoux:emerald_block")
 
 minetest.register_alias("bijoux:saphir", "bijoux:carved_sapphire")
 minetest.register_alias("bijoux:saphir_imparfait", "bijoux:not_carved_sapphire")
-minetest.register_alias("bijoux:saphir_bloc", "bijoux:carved_sapphire_block")
+minetest.register_alias("bijoux:saphir_bloc", "bijoux:sapphire_block")
 
 minetest.register_alias("bijoux:rubis", "bijoux:carved_ruby")
 minetest.register_alias("bijoux:rubis_imparfait", "bijoux:not_carved_ruby")
-minetest.register_alias("bijoux:rubis_bloc", "bijoux:carved_ruby_block")
+minetest.register_alias("bijoux:rubis_bloc", "bijoux:ruby_block")
 
 minetest.register_alias("bijoux:lapis_lazuli", "bijoux:carved_lapis_lazuli")
 minetest.register_alias("bijoux:lapis_lazuli_imparfait", "bijoux:not_carved_lapis_lazuli")
-minetest.register_alias("bijoux:lapis_lazuli_bloc", "bijoux:carved_lapis_lazuli_block")
+minetest.register_alias("bijoux:lapis_lazuli_bloc", "bijoux:lapis_lazuli_block")
 
 minetest.register_alias("bijoux:jade", "bijoux:carved_jade")
 minetest.register_alias("bijoux:jade_imparfait", "bijoux:not_carved_jade")
-minetest.register_alias("bijoux:jade_bloc", "bijoux:carved_jade_block")
+minetest.register_alias("bijoux:jade_bloc", "bijoux:jade_block")
 
 minetest.register_alias("bijoux:topaze", "bijoux:carved_topaz")
 minetest.register_alias("bijoux:topaze_imparfait", "bijoux:not_carved_topaz")
-minetest.register_alias("bijoux:topaze_bloc", "bijoux:carved_topaz_block")
+minetest.register_alias("bijoux:topaze_bloc", "bijoux:topaz_block")
 
 minetest.register_alias("bijoux:perle", "bijoux:carved_pearl")
 minetest.register_alias("bijoux:perle_imparfait", "bijoux:not_carved_pearl")
